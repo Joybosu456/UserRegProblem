513481c6cdefc68565659b59d0a1719f0a553fd2
JOY <joybosu@gmail.com>
2022-03-09 21:50:45 +0530

    pushing Uc5 Rule1

---
+++ PassWordRule1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegProblem
+{
+    public class PassWordRule1
+    {/// <summary>
+    /// UC5-PassWord Rule 1.
+    /// </summary>
+        public static string Regex_Pattern = "^[a-z]{8,}$";
+        public bool validatePasswordRule1(string passwordrule1)
+        {
+            return Regex.IsMatch(passwordrule1, Regex_Pattern);
+        }
+    }
+}
