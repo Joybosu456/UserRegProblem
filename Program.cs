07058185c11bccfaa32319d62a57936f2ad7093c
JOY <joybosu@gmail.com>
2022-03-09 21:49:50 +0530

    Adding Pass word rule 1

--- Program.cs
+++ Program.cs
@@ -7,17 +7,27 @@ namespace UserRegProblem
         {
             Console.WriteLine("Welcome to my User Regestation Problem");
 
-            MobileNo no = new MobileNo();
-            Console.WriteLine("Enter your MobileNo");
+            //MobileNo no = new MobileNo();
+            //Console.WriteLine("Enter your MobileNo");
 
-            string MobileNo = Console.ReadLine();
+            //string MobileNo = Console.ReadLine();
 
-            bool match = no.validateMobileNo(MobileNo);
+            //bool match = no.validateMobileNo(MobileNo);
 
+            //if (match)
+            //    Console.WriteLine("MobileNo Matched");
+            //else
+            //    Console.WriteLine("MobileNo are not Matche");
+
+            PassWordRule1 passwordRule1 = new PassWordRule1();
+            Console.WriteLine("Enter Password");
+            string value = Console.ReadLine();
+            bool match = passwordRule1.validatePasswordRule1(value);
             if (match)
-                Console.WriteLine("MobileNo Matched");
+                Console.WriteLine("Password Matched");
             else
-                Console.WriteLine("MobileNo are not Matche");
+                Console.WriteLine("Password Not Matched");
+
 
 
 
