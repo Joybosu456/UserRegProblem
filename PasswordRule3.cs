f365fb64d57fefad0d3877a19c6b64aefb390ce9
JOY <joybosu@gmail.com>
2022-03-09 22:13:51 +0530

    Rule no3 Program file

---
+++ PasswordRule3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegProblem
+{
+    public class PasswordRule3
+    {
+        /// <summary>
+        /// The regex pattern
+        /// UC7-Joy.
+        /// </summary>
+        public static string Regex_Pattern = "^[a-z A-Z 0-9]{8,}$";
+        public bool validatePasswordRule3(string passwordrule3)
+        {
+            return Regex.IsMatch(passwordrule3, Regex_Pattern);
+        }
+    }
+}
