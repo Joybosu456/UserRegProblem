52d53824d19951472ecca86b2b914dd962f51f07
JOY <joybosu@gmail.com>
2022-03-09 21:43:41 +0530

    pushing phone number file

---
+++ MobileNo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegProblem
+{
+    public class MobileNo
+    {
+        public static String Regex_MobileNo = "^[+91]{3}[' ']*[7-9]{1}[0-9]{9}$";
+        public bool validateMobileBNo(string MobileNo)
+        {
+            return Regex.IsMatch(MobileNo, Regex_MobileNo);
+        }
+    }
+}
