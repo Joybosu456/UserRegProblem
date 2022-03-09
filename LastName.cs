fac5e22886c85ac7b3e65e1ee2aa702f528dadcb
JOY <joybosu@gmail.com>
2022-03-09 21:17:55 +0530

    pushing last name file

---
+++ LastName.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegProblem
+{
+    public class LastName
+    {
+        /// <summary>
+        /// The regex last name
+        /// UC2-Joy
+        /// </summary>
+        public static String Regex_LastName = "^[A-Z]{1}[a-z]{3,}$";
+
+
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_LastName);
+        }
+    }
+}
